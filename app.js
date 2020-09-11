eda70d7524e7b6898fea5e3fcdcc2742e403f4e0
Ben Kelly <benkellysoftware@gmail.com>
2020-09-11 16:30:19 +1000

    Implemented subject table and creation

--- app.js
+++ app.js
@@ -1,8 +1,20 @@
 const express = require('express') // Loading in the express library
+const bodyParser = require('body-parser') // This lets us read data from post requests
 const app = express() // Creating an app
 const port = 3000 // Setting our port, 3000 is a default but feel free to change it
 
+subjects = []; // This should be a database so it can persist between restarts, but we'll get to that later
+
+function calculateWAM() {
+	//TODO: Calculate WAM
+	return 0;
+}
+
 app.use(express.static('src')); // This lets our app load in files that are in the src directory
+
+// Code I found online, I don't understand it and neither should you
+app.use(bodyParser.urlencoded({extended:true})); 
+app.use(bodyParser.json());
 
 // This is a function that will be called when the user goes to the root of the url
 app.get('/', (req, res) => {
@@ -10,11 +22,23 @@ app.get('/', (req, res) => {
 })
 
 // This request will retrieve a JSON object instead of a page
-app.get('/example', (req, res) => {
-	res.json({example: 1});
+// Note: In the future when we learn about templating and page states, this logic will be moved to the / call
+app.get('/subjects', (req, res) => {
+	res.json({
+		subjects: subjects,
+		wam: calculateWAM()
+	});
+})
+
+//This receives the subject data and adds it to the list
+app.post('/subject', (req, res) => {
+	console.log(req.body);
+	//TODO: Validate Data
+	subjects.push(req.body);
+	res.sendStatus(200); // Lets the poster know the request succeeded
 })
 
 // This sets up the app to listen to any web traffic and run the requests
 app.listen(port, () => {
-  console.log(`App listening at http://localhost:${port}`)
+  console.log(`App listening at http://localhost:${port}`);
 })
--- src/index.js
+++ src/index.js
@@ -1,6 +1,46 @@
-//TODO: Write content
-$( document ).ready(function() { // This calls when the whole html has loaded. This ensures there isn't any problem trying to access unloaded elements
-	$.get( "/example", function( data ) { // This is the frontend calling our example get request. We can use this in the future to get data from the backend dynamically
-		console.log(data);
+function refreshTable() {
+	//Returns us the subjects for our table, and our WAM
+	$.get('/subjects', function(data) { 
+		$("#subjectsTable").empty();
+
+		data.subjects.forEach(subject => {
+			$("#subjectsTable").append(
+				`<div id="subjectsTable">
+					<div class="row">
+						<span class="name">${subject.name}</span>
+						<span class="mark">${subject.mark}</span>
+						<span class="units">${subject.units}</span>
+						<button onclick="deleteSubject($(this).siblings('.name').text())">Delete</button>
+					</div>
+				</div>`
+			);
+		});
 	});
+}
+
+function createSubject() {
+	//Makes a request to add our current subject
+	$.post({
+		url: "/subject", 
+    contentType: 'application/json',
+    data: JSON.stringify({
+    	name: $("#subjectName").val(),
+    	mark: $("#subjectMark").val(),
+    	units: $("#subjectUnits").val(),
+    }),
+    success: refreshTable
+  });
+
+	$('input').val(""); // Resets the input fields so you don't create a duplicate
+}
+
+function deleteSubject(subjectName) {
+	// TODO: probably in a later workshop
+	console.log(subjectName);
+}
+
+// This calls when the whole html has loaded.
+//This ensures there isn't any problem trying to access unloaded elements.
+$( document ).ready(function() { 
+	refreshTable();
 });
